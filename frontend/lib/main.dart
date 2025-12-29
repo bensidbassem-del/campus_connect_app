35c5c06521639bb151ebfc340d93ab010d82b221
alixxxx-x <alix20041124@gmail.com>
2025-12-29 03:28:50 +0100

    new

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:campus_connect_app_login/loginstack/layerone.dart';
-import 'package:campus_connect_app_login/loginstack/layertwo.dart';
-import 'package:campus_connect_app_login/loginstack/layerthree.dart';
+
 
 void main() {
   runApp(MaterialApp(
@@ -15,37 +13,75 @@ void main() {
 }
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.symmetric(vertical: 30),
+        width: double.infinity,
         decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('images/primaryBg.png'),
-              fit: BoxFit.cover,
-            )),
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-                top: 120,
-                left: 30,
-                child: Container(
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                        fontSize: 68,
-                        fontFamily: 'Poppins-Medium',
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white),
-                  ),
-                )),
-            Positioned(top: 210, right: 0, bottom: 0, child: LayerOne()),
-            Positioned(top: 238, right: 0, bottom: 28, child: LayerTwo()),
-            Positioned(top: 240, right: 0, bottom: 38, child: LayerThree()),
-          ],
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors:[
+              Colors.orange[900]!,
+              Colors.orange[400]!
+
+            ]
+          )
         ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),),
+                SizedBox(height: 10,),
+                Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),),
+              ],),
+            ),
+            SizedBox(height: 20,),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(60),topRight: Radius.circular(60),)
+                ),
+                child: Padding(
+                 padding: EdgeInsets.all(30), 
+                 child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 60,),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                          color: Color.fromRGBO(225, 95, 27, .3),
+                          blurRadius: 20,
+                          offset: Offset(0, 10),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                 ),
+                ),
+              ),
+            ),
+          ],
+        )
       ),
     );
   }
